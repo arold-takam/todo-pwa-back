b7285037d9abb193e5e740e3329ba3f20c9eb2f8
arold-takam <www.aroldtakam3@gmail.com>
2026-03-21 05:19:05 +0100

    fix: déplacement bootstrap dans package scannable + log démarrage visible

--- application/src/main/java/com/app/TodoApplication.java
+++ application/src/main/java/com/app/TodoApplication.java
@@ -3,12 +3,13 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
 public class TodoApplication {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 }
